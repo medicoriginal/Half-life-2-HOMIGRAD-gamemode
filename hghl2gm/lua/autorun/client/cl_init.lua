c2a9f001a499857749b51390ea7005e44d58bd6b
medicoriginal <113814703+medicoriginal@users.noreply.github.com>
2025-01-16 19:22:47 +0700

    Update cl_init.lua

--- hghl2gm/lua/autorun/client/cl_init.lua
+++ hghl2gm/lua/autorun/client/cl_init.lua
@@ -37,20 +37,20 @@ net.Receive("hl2start", function()
 hook.Add( "HUDPaint", "start", function()	
 	surface.SetDrawColor( 0, 0, 0, 254 )
 	surface.DrawRect( 0, 0, ScrW(), ScrH() )
-	draw.DrawText( "НАЧИНАЕМ РЕЖИМ HALF LIFE 2 COOP", "fonta", ScrW() * 0.5, ScrH() * 0.25, color_white, TEXT_ALIGN_CENTER )
-	draw.DrawText( "Лидер отряда повстанцев: " .. Entity( 1 ):GetName(), "fontb", ScrW() * 0.5, ScrH() * 0.35, color_white, TEXT_ALIGN_CENTER )
-	draw.DrawText( "Название отряда повстанцев: " .. GetHostName(), "fontb", ScrW() * 0.5, ScrH() * 0.45, color_white, TEXT_ALIGN_CENTER )
+	draw.DrawText( "STARTING GAMEMODE HALF LIFE 2 COOP", "fonta", ScrW() * 0.5, ScrH() * 0.25, color_white, TEXT_ALIGN_CENTER )
+	draw.DrawText( "Leader of resistance squad: " .. Entity( 1 ):GetName(), "fontb", ScrW() * 0.5, ScrH() * 0.35, color_white, TEXT_ALIGN_CENTER )
+	draw.DrawText( "Name of resistance squad: " .. GetHostName(), "fontb", ScrW() * 0.5, ScrH() * 0.45, color_white, TEXT_ALIGN_CENTER )
 
 
 
 	hook.Add("HUDPaint", "STATS" , function () 
-	draw.SimpleText( "Режим: COOP HL2", "CloseCaption_Bold", 3, 0, color_white, 0, TEXT_ALIGN_TOP )
+	draw.SimpleText( "Gamemode: COOP HL2", "CloseCaption_Bold", 3, 0, color_white, 0, TEXT_ALIGN_TOP )
 	
 --я из прошлого:сделай тут название карты уровня тип 21:56 23.12.24
 --будущий я:ок понял 21:57 23.12.24
 
 
-draw.SimpleText( "Уровень: " .. game.GetMap(), "CloseCaption_Bold", 3, ScrH() / 45, color_white)
+draw.SimpleText( "Level: " .. game.GetMap(), "CloseCaption_Bold", 3, ScrH() / 45, color_white)
 --я сейчас (который из будущего типо):вот мой павелитель 15:53 24.12.24
 --я сейчас (который из будущего будущего):а чужие переписки читать плохо!! 21:53 25.12.24
 
